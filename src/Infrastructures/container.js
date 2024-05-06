d95a0deb7c8da66967c38a41b8cda17a87fcf04e
unknown <yyongkisetiawan@gmail.com>
2024-05-06 22:53:04 +0700

    Membuat Service Locator

---
+++ src/Infrastructures/container.js
@@ -0,0 +1,66 @@
+/* istanbul ignore file */
+
+const { createContainer } = require('instances-container');
+
+// external agency
+const { nanoid } = require('nanoid');
+const bcrypt = require('bcrypt');
+const pool = require('./database/postgres/pool');
+
+// service (repository, helper, manager, ect)
+const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
+const BcryptPasswordHash = require('./security/BcryptPasswordHash');
+
+// use case
+const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
+const UserRepository = require('../Domains/users/UserRepository');
+const PasswordHash = require('../Applications/scurity/PasswordHash');
+
+// creating container
+const container = createContainer();
+
+// registering service and repository
+container.register([
+  {
+    key: UserRepository.name,
+    class: UserRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        {
+          concrete: pool,
+        },
+        {
+          concrete: nanoid,
+        },
+      ],
+    },
+  },
+  {
+    key: PasswordHash.name,
+    class: BcryptPasswordHash,
+    parameter: {
+      dependencies: [
+        {
+          concrete: bcrypt,
+        },
+      ],
+    },
+  },
+]);
+
+// registering use cases
+container.register([
+  {
+    key: AddUserUseCase.name,
+    class: AddUserUseCase,
+    parameter: {
+      injectTypeL: 'destructuring',
+      dependencies: [
+        {
+          name: 'userRepository',
+          internal: UserRepository.name,
+        },
+      ],
+    },
+  },
+]);
